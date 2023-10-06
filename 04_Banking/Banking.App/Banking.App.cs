eeca47d6a62d375e3132c167a1776233c3abd0b2
leolileoo <145107070+leolileoo@users.noreply.github.com>
2023-10-06 12:50:29 +0200

    Add files via upload

---
+++ 04_Banking/Banking.App/Banking.App.cs
@@ -0,0 +1,55 @@
+﻿using Banking.Logic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Account account = null!;
+        System.Console.Write("Type of account ([c]hecking, [b]usiness, [s]avings)  > ");
+        bool @continue = true;
+        while (@continue)
+        {
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.C:
+                    System.Console.Write("Checking account");
+                    account = new CheckingAccouint();
+                    @continue = false;
+                    break;
+                case ConsoleKey.B:
+                    System.Console.Write("Business account");
+                    account = new BusinessAccount();
+                    @continue = false;
+                    break;
+                case ConsoleKey.S:
+                    System.Console.Write("Savings account");
+                    account = new SavingsAccount();
+                    @continue = false;
+                    break;
+            }
+        }
+        System.Console.WriteLine();
+        System.Console.Write("Account number                                       > "); Console.ReadLine();
+        System.Console.Write("Account holder                                       > "); Console.ReadLine();
+        System.Console.Write("Current balance                                      > "); account.currentBalance = Convert.ToDouble(Console.ReadLine());
+        System.Console.Write("Transaction account number                           > "); Console.ReadLine();
+        System.Console.Write("Transaction description                              > "); Console.ReadLine();
+        System.Console.Write("Transaction amount                                   > "); account.transactionAmount = Convert.ToDouble(Console.ReadLine());
+        System.Console.Write("Transaction timestamp                                > ");
+        var key = Console.ReadKey(true);
+        if (key.Key == ConsoleKey.Spacebar)
+        {
+            System.Console.WriteLine(DateTime.Now);
+        }
+        else
+        {
+            System.Console.Write(key.KeyChar);
+            Console.ReadLine();
+        }
+
+        if (account.TryToExecute())
+            System.Console.WriteLine($"Transaction executed successfully. The new current balance is {account.currentBalance:f2}€.");
+        else
+            System.Console.WriteLine("Transaction not allowed.");
+    }
+}
---
+++ 04_Banking/Banking.Logic/Banking.Logic.cs
@@ -0,0 +1,66 @@
+﻿namespace Banking.Logic;
+
+public abstract class Account 
+{
+    public double currentBalance { get; set; }
+    public double transactionAmount { get; set; }
+
+    public abstract bool TryToExecute();
+}
+
+public class CheckingAccouint : Account
+{
+    public override bool TryToExecute()
+    {
+        if (transactionAmount <= 100000)
+        {
+            var newBalance = currentBalance + transactionAmount;
+
+            if (10000000 > newBalance && newBalance > -10000)
+            {
+                currentBalance = newBalance;
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+public class BusinessAccount : Account
+{
+    public override bool TryToExecute()
+    {
+        if (transactionAmount <= 100000)
+        {
+            var newBalance = currentBalance + transactionAmount;
+
+            if (100000000 >= newBalance && newBalance >= -1000000)
+            {
+                currentBalance = newBalance;
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+public class SavingsAccount : Account
+{
+    public override bool TryToExecute()
+    {
+        if (transactionAmount <= 100000)
+        {
+            var newBalance = currentBalance + transactionAmount;
+
+            if (10000000 >= newBalance && newBalance >= 0)
+            {
+                currentBalance = newBalance;
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
